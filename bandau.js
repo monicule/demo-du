c05acad03d6d09dd160a1c65a3fb29862593a9e5
Monika <laiskutis.monikai@gmail.com>
2024-05-02 11:19:03 +0300

    update

--- bandau.js
+++ bandau.js
@@ -1,3 +1,15 @@
-let x = 0;
+let z = 0;
 
-console.log(x);
+console.log(z);
+
+let x = 100;         // x is a number
+
+let y = "100";       // y is a string
+
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes
+
+console.log(carName1);
+console.log(carName2);
+
+
